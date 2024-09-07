6b8a1323dcfff73dc22564a463af6b6e0ae7067b
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-07 11:25:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,7 @@ import random
 English = {
     "brittle": "friabile",
     "whizz":"sfrecciare",
+    "squish":"spremere, strizzare",
     "dredge up": "rivangare",
     "declutter": "riordinare",
     "heckle":"infastidire",
