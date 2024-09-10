ca85b4edcfeed38fc44606279fd0e1c0befc92b6
Mattia De Tommaso <91329796+leone2497@users.noreply.github.com>
2024-09-10 23:10:07 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import random
 # Define the dictionaries
 English = {
     "brittle": "friabile",
+    "disgress":"divagare",
     "harzardous":"pericolos",
     "berate":"rimproverare",
     "pelvis":"bacino",
